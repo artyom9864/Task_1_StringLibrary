4f13604ee63d57bce1069873204822f9cb77759c
Артём Братчук <artyom9864@mail.ru>
2021-01-05 20:49:28 +0300

    Добавьте файлы проекта.

---
+++ StringLibrary/HelloMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StringLibrary
+{
+    public class HelloMessage
+    {
+        public string GetHelloMessage(string username)
+        {
+            return DateTime.Now.ToString() + " Hello " + username;    
+        }
+
+    }
+}
